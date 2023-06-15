1d95b1e799eb425fbbe802c6702b3030672201a5
pabloalban <pablo.alban@iess.gob.ec>
2023-06-15 11:38:05 -0500

    Tablas de impacto

--- R/RTR/513_tab_indemnizaciones.R
+++ R/RTR/513_tab_indemnizaciones.R
@@ -9,7 +9,9 @@ source( 'R/500_tildes_a_latex.R', encoding = 'UTF-8', echo = FALSE )
 message( '\tTablas de afiliados afectados al tope por 60, 50 y 40 sbu' )
 #Tabla de afiliados afectados al tope de 60 sbu-----------------------------------------------------
 
-aux <- tab_ind_max %>% clean_names( )
+aux <- tab_ind_max %>% 
+  clean_names( ) %>%
+  mutate_at( c(2:4), as.integer )
 
 aux_xtab <- xtable( aux, digits = c( 0, rep(0, 4), rep( 2, 4 ) ) )
 
@@ -25,7 +27,9 @@ print( aux_xtab,
 
 #Tabla de afiliados afectados al tope de 50 sbu-----------------------------------------------------
 
-aux <- tab_ind_max_50 %>% clean_names( )
+aux <- tab_ind_max_50 %>% 
+  clean_names( ) %>%
+  mutate_at( c(2:4), as.integer )
 
 aux_xtab <- xtable( aux, digits = c( 0, rep(0, 4), rep( 2, 4 ) ) )
 
@@ -42,12 +46,31 @@ print( aux_xtab,
 
 #Tabla de afiliados afectados al tope de 40 sbu-----------------------------------------------------
 
-aux <- tab_ind_max_40 %>% clean_names( )
+aux <- tab_ind_max_40 %>% 
+  clean_names( ) %>%
+  mutate_at( c(2:4), as.integer )
 
 aux_xtab <- xtable( aux, digits = c( 0, rep(0, 4), rep( 2, 4 ) ) )
 
 print( aux_xtab,
        file = paste0( parametros$resultado_tablas, 'tab_ind_max_40', '.tex' ),
+       type = 'latex',
+       include.colnames = FALSE, include.rownames = FALSE,
+       format.args = list( decimal.mark = ',', big.mark = '.' ),
+       only.contents = TRUE,
+       hline.after = c( nrow( aux ) - 1,
+                        nrow( aux ) ),
+       sanitize.text.function = identity )
+
+#Tabla del impacto monetario------------------------------------------------------------------------
+
+aux <- tab_impacto %>% 
+  clean_names( )
+
+aux_xtab <- xtable( aux, digits = c( 0, 0, rep( c(0,2), 3 ) ) )
+
+print( aux_xtab,
+       file = paste0( parametros$resultado_tablas, 'tab_impacto', '.tex' ),
        type = 'latex',
        include.colnames = FALSE, include.rownames = FALSE,
        format.args = list( decimal.mark = ',', big.mark = '.' ),
